7d3844a96e529557a2fa186497bb14bdd09a1416
Gorrie Coe <gorriecoe@gmail.com>
2018-11-25 13:09:08 +1300

    Added documentation for html toggler

--- src/9-plugins/toggler/options/html.js
+++ src/9-plugins/toggler/options/html.js
@@ -1,9 +1,38 @@
+// HTML
+//
+// Toggles the inner html of an element between itself and the given element identified by HTML ID
+//
+// @todo Complete
+//
+// Markup:
+// <div data-click="trigger: #site-search; html: #header-search-inactive, #header-search-active">
+//   <div class="button">
+//     <span class="ri ri-chevron-down"></span>
+//     <span>
+//       Search
+//     </span>
+//   </div>
+// </div>
+// <script type="text/html" id="header-search-inactive">
+//   <div class="button">
+//     <span class="ri ri-chevron-down"></span>
+//     <span>
+//       Search
+//     </span>
+//   </div>
+// </script>
+// <script type="text/html" id="header-search-active">
+//   <div class="button-icon">
+//     <span class="ri ri-chevron-up"></span>
+//   </div>
+// </script>
+//
+// Styleguide: Plugins.Toggler.HTML
+
 export default function (owner, state, variables) {
   if (state) {
-    $(owner).data('template', variables[1])
     $(owner).html($(variables[1]).html())
   } else {
-    $(owner).data('template', variables[0])
     $(owner).html($(variables[0]).html())
   }
 }
