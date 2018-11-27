54218523b7555dfd186f364fa8b65f4cd9d9b9b1
Gorrie Coe <gorriecoe@gmail.com>
2018-11-28 10:29:36 +1300

    Fixed undefined tipInstance

--- src/9-plugins/clipboard/index.js
+++ src/9-plugins/clipboard/index.js
@@ -15,7 +15,7 @@ import tooltip from 'tooltip.js'
 
 export default function () {
   $('[data-clipboard]').each(function(index, element){
-    tipInstance = new tooltip(element, {
+    const tipInstance = new tooltip(element, {
       title: 'Copied!',
       placement: 'top',
       trigger: 'manual'
